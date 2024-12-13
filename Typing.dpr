93d34fa171c623ddbde981cf963f91e8ed0d2d6b
Danil <64485105+Vindicativva@users.noreply.github.com>
2024-12-13 22:59:07 +0300

    Add Language function

--- Typing.dpr
+++ Typing.dpr
@@ -1,12 +1,34 @@
 program Typing;
 
-{$APPTYPE CONSOLE}
-
-{$R *.res}
-
 uses
-  System.SysUtils;
+  Windows,
+  SysUtils;
+
+var
+  n: integer;
+  lang: string;
+
+function GetWordFromFile(FileName: string; Number: integer): String;
+  var
+    WordFile: TextFile;
+    s: String;
+    arr: TArray<String>;
+  begin
+    AssignFile(WordFile, FileName);
+    Reset(WordFile);
+    ReadLn(WordFile, s);
+    Randomize;
+    arr := s.Split([' ']);
+    CloseFile(WordFile);
+    Result := arr[random(Length(arr))];
+  end;
 
 begin
+  lang := 'Rus';
+  n := 10;
+  Writeln(GetWordFromFile('..\..\' + lang + '\' + lang + IntToStr(n) +'.txt', n));
 
+  Writeln;
+  Writeln('Введите Enter для выхода...');
+  Readln;
 end.
