3cdf8039f23f0849b4d27ca5378fedca2c906cb7
frankdroom <151196398+frankdroom@users.noreply.github.com>
2024-12-13 12:18:39 +0300

    Update Typing.dpr

--- Typing.dpr
+++ Typing.dpr
@@ -1,12 +1,42 @@
-program Typing;
-
-{$APPTYPE CONSOLE}
-
-{$R *.res}
-
-uses
-  System.SysUtils;
-
+procedure ClearConsole;
+var
+  Handle: THandle;
+  ConsoleInfo: CONSOLE_SCREEN_BUFFER_INFO;
+  ConsoleSize, CharsWritten: DWORD;
+  TopLeft: TCoord;
 begin
+  {$IFDEF MSWINDOWS}
+  Handle := GetStdHandle(STD_OUTPUT_HANDLE);
+  if Handle <> INVALID_HANDLE_VALUE then
+  begin
+    // Get console screen buffer info
+    if GetConsoleScreenBufferInfo(Handle, ConsoleInfo) then
+    begin
+      ConsoleSize := ConsoleInfo.dwSize.X * ConsoleInfo.dwSize.Y;
+      TopLeft.X := 0;
+      TopLeft.Y := 0;
 
-end.
+      // Fill the console with spaces
+      FillConsoleOutputCharacter(Handle, ' ', ConsoleSize, TopLeft, CharsWritten);
+
+      // Reset the console's attributes
+      FillConsoleOutputAttribute(Handle, ConsoleInfo.wAttributes, ConsoleSize, TopLeft, CharsWritten);
+
+      // Move the cursor to the top left
+      SetConsoleCursorPosition(Handle, TopLeft);
+    end;
+
+
+  end;
+
+
+  {$ENDIF}
+end;
+
+
+procedure new_round(var round_num :integer);
+begin
+  inc(round_num);
+  ClearConsole;
+  writeln('Раунд ', round_num);
+end;
