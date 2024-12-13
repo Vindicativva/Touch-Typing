497db355c27e0d9baeda3f6902f38741ac2c175b
Uiyrte1 <timofey.kurstak@yandex.by>
2024-12-13 15:07:59 +0300

    Update Russian Words

--- Typing.dpr
+++ Typing.dpr
@@ -1,13 +1,13 @@
 ﻿program Typing;
 
 uses
-  Windows,System.SysUtils;
+  System.SysUtils;
 
 Type
-  Dc = Array Of Array of String;
+  Dc = Array Of Array Of Array of String;
 const
-  Dicti: Dc =
-  [['a'],
+  Dicti: Dc = [
+  [['a', 'i'],
    ['at', 'to', 'in', 'on', 'by', 'he', 'we', 'it', 'an', 'be', 'do', 'up', 'no', 'so', 'go', 'is', 'me', 'my', 'if', 'of'],
    ['cat', 'dog', 'hat', 'sun', 'run', 'red', 'bed', 'bag', 'map', 'key', 'box', 'top', 'fan', 'pen', 'cup', 'car', 'leg', 'sit', 'win', 'kit'],
    ['book', 'cake', 'door', 'fish', 'game', 'hand', 'idea', 'jump', 'king', 'lamp', 'moon', 'nest', 'page', 'rain', 'star', 'tree', 'wave', 'wind', 'word', 'year'],
@@ -16,32 +16,44 @@ const
    ['already', 'balance', 'capital', 'feature', 'grammar', 'freedom', 'general', 'history', 'imagine', 'journey', 'kitchen', 'library', 'measure', 'natural', 'opinion', 'prepare', 'quality', 'respect', 'support', 'trouble'],
    ['accurate', 'behavior', 'birthday', 'category', 'decision', 'elephant', 'feedback', 'hospital', 'internet', 'location', 'material', 'notebook', 'opposite', 'possible', 'question', 'recovery', 'tomorrow', 'universe', 'vertical', 'wildlife'],
    ['attention', 'brilliant', 'confusion', 'education', 'expansion', 'generator', 'happiness', 'languages', 'marketing', 'notorious', 'operation', 'potential', 'reception', 'selection', 'technical', 'universal', 'workforce', 'telephone', 'universal'],
-   ['accomplish', 'deliberate', 'endangered', 'foundation', 'generation', 'helicopter', 'impossible', 'journalism', 'literature', 'motivation', 'playground', 'revolution', 'surprising', 'blackberry', 'developing', 'expedition', 'television', 'uplifting', 'prosperous', 'management']];
+   ['accomplish', 'deliberate', 'endangered', 'foundation', 'generation', 'helicopter', 'impossible', 'journalism', 'literature', 'motivation', 'playground', 'revolution', 'surprising', 'blackberry', 'developing', 'expedition', 'television', 'uplifting', 'prosperous', 'management']],
+  [['я', 'и', 'с'],
+   ['да', 'по', 'не'],
+   ['дом', 'лес', 'сад'],
+   ['лето', 'вода', 'тень'],
+   ['огонь', 'вагон', 'осень'],
+   ['листья', 'облако', 'костёр'],
+   ['природа', 'прудики', 'зеркало'],
+   ['ожерелья', 'ледников', 'тропинка'],
+   ['гроиадный', 'зачарован', 'мировозар'],
+   ['абсолютизм', 'абстракция', 'авиасервис']]];
 
 var
   s: String;
   l: integer;
 
+
 procedure f1(const Dic: Dc; var str: string; k: integer);
 var
-  l: integer;
+  l, f: integer;
   word: string;
 begin
   Randomize;
+  f:= Random(2);
   while length(Str) < k do
   begin
     if k >= 9 then
       l := Random(9)
     else
       l := Random(k);
-    word:= Dic[l][Random(length(Dic[l]))];
+    word:= Dic[f][l][Random(length(Dic[f][l]))];
     if Random(5)  = 0 then
       Word[1] := chr(Ord(Word[1]) - 32);
 
     if ((k - length(Str) - l) <= 6) and ((k - length(Str) - (l + 1)) >= 2) then
     begin
        Str:= Str + word + ' ';
-       Str:= Str + Dic[k - length(Str)-1][Random(length(Dic[k - length(Str)-1]))];
+       Str:= Str + Dic[f][k - length(Str)-1][Random(length(Dic[f][k - length(Str)-1]))];
     end
     else if (k - length(Str) - (l + 1)) > 6 then
        Str:= Str + word + ' ';
